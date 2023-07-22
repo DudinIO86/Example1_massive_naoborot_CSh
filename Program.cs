7db2214cfdbb14150ab036412c6159e021401640
Igor Dudin <dudinio@mail.ru>
2023-07-22 21:50:55 +0300

    Работающий код

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿//блок исходных данных
-int []arrow={2,1,2,3,4,5};
+int []arrow={5,1,2,3,4,7,55,4};
+
 int size=arrow.Length;
+int []arrow2=new int[size];
 int index=0;
-int t1=arrow[0];
 
 //блок операций
 while(index<size)
 {
-    arrow[index]=arrow[size-index-1];
-    arrow[size-1]=t1;
-    index++;
+   arrow2[index]=arrow[size-index-1];
+   index++;
 }
 
 //вывод на печать
 var str=string.Join(" ",arrow);
+var str2=string.Join(" ",arrow2);
 
 Console.WriteLine(str);
+Console.WriteLine(str2);
