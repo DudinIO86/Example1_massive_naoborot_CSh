f8c558634c768b00938595042bbc7c5dab293a19
Igor Dudin <dudinio@mail.ru>
2023-07-22 21:12:08 +0300

    Написан код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//блок исходных данных
+int []arrow={2,1,2,3,4,5};
+int size=arrow.Length;
+int index=0;
+int t1=arrow[0];
+
+//блок операций
+while(index<size)
+{
+    arrow[index]=arrow[size-index-1];
+    arrow[size-1]=t1;
+    index++;
+}
+
+//вывод на печать
+var str=string.Join(" ",arrow);
+
+Console.WriteLine(str);
